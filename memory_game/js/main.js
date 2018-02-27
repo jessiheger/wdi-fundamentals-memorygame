deb40e936bc4d9ec8592dc7e43a5055d9a6154d6
Jessica Heger <jhmheger@gmail.com>
2018-02-27 14:55:10 +0530

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 // console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -8,16 +30,21 @@ var checkForMatch = function() {
 	alert("You found a match!");
 	} else {
 	alert("Sorry, try again.");
-	}	
+	}
 };
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	if (cardsInPlay.length === 2) {
 	checkForMatch();
+}
 };
 flipCard(0);
 flipCard(2);
+
 /*
 	var cardOne = cards[0];
 cardsInPlay.push(cardOne);
@@ -26,7 +53,4 @@ console.log("User flipped queen");
 	var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
-};
 */
